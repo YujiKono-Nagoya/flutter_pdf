1a802c665f5c8c199161f0a8c0ac85eaf51d504e
YujiKono-Nagoya <yuji.kono.19@gmail.com>
2023-10-08 17:02:42 +0900

    2023/10/08

---
+++ lib/page/pdf_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+import '../api/pdf_api.dart';
+import '../api/pdf_paragraph_api.dart';
+import '../main.dart';
+import '../widget/button_widget.dart';
+
+class PdfPage extends StatefulWidget {
+  @override
+  _PdfPageState createState() => _PdfPageState();
+}
+
+class _PdfPageState extends State<PdfPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(MyApp.title),
+          centerTitle: true,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(32),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ButtonWidget(
+                  text: 'Simple PDF',
+                  onClicked: () async {
+                    final pdfFile =
+                        await PdfApi.generateCenteredText('Sample Text');
+
+                    PdfApi.openFile(pdfFile);
+                  },
+                ),
+                const SizedBox(height: 24),
+                ButtonWidget(
+                  text: 'Paragraphs PDF',
+                  onClicked: () async {
+                    final pdfFile = await PdfParagraphApi.generate();
+                    final Email email = Email(
+                      body: '日報送信',
+                      subject: '日報送信',
+                      recipients: ['aaa@gmail.com'],
+                    );
+
+                    PdfApi.openFile(pdfFile);
+
+                    // await FlutterEmailSender.send(email);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+}
